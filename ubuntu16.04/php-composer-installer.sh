ba6c27bb76ee4852927c69811f406105693fb233
Fu-Kai Hu <chaos201388@gmail.com>
2018-05-28 07:15:27 +0800

    Added php-xdebug

--- ubuntu16.04/php-composer-installer.sh
+++ ubuntu16.04/php-composer-installer.sh
@@ -11,7 +11,13 @@ apt policy | grep php && \
     apt show php7.2
 
 sudo apt install -y php7.2 php7.2-fpm \
-    php7.2-bcmath php7.2-curl php7.2-mbstring php7.2-mysql php7.2-zip php7.2-xml
+    php7.2-bcmath \
+    php7.2-curl \
+    php7.2-mbstring \
+    php7.2-mysql \
+    php7.2-xdebug \
+    php7.2-xml \
+    php7.2-zip
 
 sudo sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php/7.2/fpm/php.ini && \
     sudo systemctl restart php7.2-fpm
