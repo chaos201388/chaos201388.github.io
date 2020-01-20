2077366a00bfff25604a2976235afc00535c3470
Fu-Kai Hu <chaos201388@gmail.com>
2020-01-20 22:41:06 +0800

    Rename files

--- ubuntu/18.04/subversion/installer.sh
+++ ubuntu/18.04/subversion/installer.sh
@@ -5,8 +5,11 @@
 
 sudo apt update -y
 sudo apt install apache2 subversion libapache2-mod-svn -y
+
 sudo mkdir -p /opt/svn
+
 sudo svnadmin create /opt/svn/demo
 sudo chown -R www-data:www-data /opt/svn/demo
 sudo chmod -R 775 /opt/svn/demo
+
 sudo htpasswd -cm /etc/svn-auth-users demouser
