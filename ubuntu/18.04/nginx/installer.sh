0e614e1c9993126653e829a05de6dd6f7dec70ed
Fu-Kai Hu <chaos201388@gmail.com>
2020-01-20 23:25:08 +0800

    Added nginx installer

---
+++ ubuntu/18.04/nginx/installer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt update -y
+sudo apt install nginx -y
+
+# sudo systemctl status apache2
+# sudo systemctl disable apache2.service
+# sudo systemctl stop apache2.service
+
+# sudo systemctl status nginx.service
+# sudo systemctl enable nginx.service
+# sudo systemctl start nginx.service
+
+# sudo ufw status verbose
+# sudo ufw allow 'Nginx HTTP'
