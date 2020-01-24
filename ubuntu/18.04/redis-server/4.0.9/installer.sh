8651dd0a46586001bd154cb7b8782917ee9ba0f6
Fu-Kai Hu <chaos201388@gmail.com>
2020-01-24 23:33:27 +0800

    Added installer and configuration for redis server 4.0.9

---
+++ ubuntu/18.04/redis-server/4.0.9/installer.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+##############################################
+### redis-server 4.0.9 and redis-cli 4.0.9 ###
+##############################################
+
+sudo apt update -y
+sudo apt install redis-server -y
+
+# sudo apt list --installed | grep redis
+# sudo apt purge redis* -y
+# sudo apt autoremove -y
+
+https://websiteforstudents.com/how-to-install-and-configure-server-redis-server-on-ubuntu-18-04-16-04/
+
+https://linuxize.com/post/how-to-install-and-configure-redis-on-ubuntu-18-04/
+
+https://www.howtoforge.com/tutorial/how-to-install-and-secure-redis-on-ubuntu-18-04/
+
+### 1. Install redis-server (and redis-cli)
+### 2. setup redis.conf (disable command..)
+### 3. setup firewall for redis server
