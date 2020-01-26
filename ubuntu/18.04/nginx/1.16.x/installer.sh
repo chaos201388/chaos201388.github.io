672a31a78b7df586bbc2744ae89917f4ddfab003
Fu-Kai Hu <chaos201388@gmail.com>
2020-01-26 21:48:15 +0800

    Updated installer and source list for nginx

---
+++ ubuntu/18.04/nginx/1.16.x/installer.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+### https://nginx.org/en/linux_packages.html#Ubuntu
+
+# sudo apt install curl gnupg2 ca-certificates lsb-release
+# echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+# curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+# sudo apt-key fingerprint ABF5BD827BD9BF62
+# sudo apt update
+# sudo apt install nginx
+
+
+# sudo systemctl status nginx.service
+# sudo systemctl enable nginx.service
+# sudo systemctl start nginx.service
+
+# sudo ufw status verbose
+# sudo ufw allow 'Nginx HTTP'
+
+
+# sudo apt purge nginx*
+# sudo apt autoremove
